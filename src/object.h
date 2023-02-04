4d4aa24e283fe7ece2b362b7662f3cfa2d7b590f
Rob <69918580+bruhmoent@users.noreply.github.com>
2023-02-04 11:09:22 +0100

    Update object.h

--- src/object.h
+++ src/object.h
@@ -11,6 +11,21 @@ public:
 	void setVelocity(Object& object, sf::Vector2f newVelocity);
 	sf::RectangleShape getShape() const;
 	virtual void createObject(int width, int length, int xstart, int ystart, int r, int g, int b, int type);
+	virtual void deleteObject(sf::Vector2f mousePos, int type);
 	virtual void applyGravity(Object& object, float gravity, float velocityLimit);
 	virtual void applyVerticalMovement(Object& object, float x, float velocityLimit);
+};
+
+class Camera
+{
+public:
+    Camera() : m_position(0.0f, 0.0f), m_movement(0.0f, 0.0f) {}
+	void update(sf::Time deltaTime);
+	void setMovement(sf::Vector2f movement);
+	void setPosition(sf::Vector2f position);
+	sf::Vector2f getMovement() const;
+	sf::Vector2f getPosition() const;
+private:
+    sf::Vector2f m_position;
+    sf::Vector2f m_movement;
 };
